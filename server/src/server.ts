402d65020374516ffb10333793bbcc5d77c10d29
Alexander <65725016+chillhaze@users.noreply.github.com>
2022-04-26 23:47:11 +0300

    added cors

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,7 +2,7 @@ import bodyParser from "body-parser";
 import express from "express";
 import connectDB from "../config/database";
 import AppRouter from "./routes";
-// import cors from "cors";
+import cors from "cors";
 
 const app = express();
 const router = new AppRouter(app);
@@ -11,7 +11,7 @@ connectDB();
 
 // Express configuration
 app.set("port", process.env.PORT || 5000);
-// app.use(cors());
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
