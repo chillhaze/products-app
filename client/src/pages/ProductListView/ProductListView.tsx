0e0589a3c2745c0d13f926229b4a0923b9620d90
Alexander <65725016+chillhaze@users.noreply.github.com>
2022-04-26 23:36:05 +0300

    headers changed

--- client/src/pages/ProductListView/ProductListView.tsx
+++ client/src/pages/ProductListView/ProductListView.tsx
@@ -51,7 +51,10 @@ const ProductListView = () => {
   };
 
   const handleMassDelete = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
     productsToDelete?.forEach((id) => productsService.deleteProduct(id));
+
+    getProducts();
   };
 
   return (
--- server/src/controllers/products.controller.ts
+++ server/src/controllers/products.controller.ts
@@ -40,7 +40,7 @@ export class ProductsController {
       message: `Product [id:'${id}'] deleted`,
       data: deletedProduct,
     });
-    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000/");
     res.setHeader("Access-Control-Allow-Credentials", "true");
     res.setHeader("Access-Control-Max-Age", "1800");
     res.setHeader("Access-Control-Allow-Headers", "content-type");
