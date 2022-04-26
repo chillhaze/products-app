d728be8a3300e5ef2e37c99146eee16b67a396b8
Alexander <65725016+chillhaze@users.noreply.github.com>
2022-04-26 15:57:08 +0300

    db-connection fix

--- server/config/database.ts
+++ server/config/database.ts
@@ -1,21 +1,28 @@
 import config from "config";
 import { ConnectOptions, connect } from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 
 type ConnectionOptionsExtend = {
   useNewUrlParser: boolean;
   useUnifiedTopology: boolean;
 };
 
+const { mongoURI: DB_URI } = process.env;
+
 const connectDB = async () => {
   try {
-    const mongoURI: string = config.get("mongoURI");
+    const mongoURI: string | undefined = config.get("mongoURI") || DB_URI;
     const options: ConnectOptions & ConnectionOptionsExtend = {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     };
-    await connect(mongoURI, options);
-    // tslint:disable-next-line:no-console
-    console.log("MongoDB Connected...");
+
+    if (mongoURI) {
+      await connect(mongoURI, options);
+      // tslint:disable-next-line:no-console
+      console.log("MongoDB Connected...");
+    }
   } catch (err) {
     // tslint:disable-next-line:no-console
     let errorMessage = "Failed to do something exceptional";
