8797e2517f1d58f3d8a68336333e31922cf8cf07
Alexander <65725016+chillhaze@users.noreply.github.com>
2022-04-26 23:22:56 +0300

    request headers added

--- server/src/controllers/products.controller.ts
+++ server/src/controllers/products.controller.ts
@@ -40,6 +40,14 @@ export class ProductsController {
       message: `Product [id:'${id}'] deleted`,
       data: deletedProduct,
     });
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Credentials", "true");
+    res.setHeader("Access-Control-Max-Age", "1800");
+    res.setHeader("Access-Control-Allow-Headers", "content-type");
+    res.setHeader(
+      "Access-Control-Allow-Methods",
+      "PUT, POST, GET, DELETE, PATCH, OPTIONS",
+    );
   }
 }
 
