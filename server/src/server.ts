0ba5a37fcbd01dbb848f0757e7293f9fa1ff0dec
Alexander <65725016+chillhaze@users.noreply.github.com>
2022-04-26 23:50:48 +0300

    added cors options

--- server/src/server.ts
+++ server/src/server.ts
@@ -4,6 +4,11 @@ import connectDB from "../config/database";
 import AppRouter from "./routes";
 import cors from "cors";
 
+const corsOptions = {
+  origin: "https://products-app-scndwb.netlify.app/",
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
 const app = express();
 const router = new AppRouter(app);
 // Connect to MongoDB
@@ -11,7 +16,7 @@ connectDB();
 
 // Express configuration
 app.set("port", process.env.PORT || 5000);
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
