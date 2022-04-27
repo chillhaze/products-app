5bd5c73283829c0202d9b4e3158a2b08c8356138
Alexander <65725016+chillhaze@users.noreply.github.com>
2022-04-27 10:15:21 +0300

    fixed db url

--- client/src/consts/base-url.ts
+++ client/src/consts/base-url.ts
@@ -1,4 +1,3 @@
-const BASE_URL = 'http://localhost:5000';
-// const BASE_URL = 'https://mysterious-wildwood-80076.herokuapp.com';
+const BASE_URL = 'https://mysterious-wildwood-80076.herokuapp.com';
 
 export default BASE_URL;
